ba0e470223d51eb49601b61a858ecc407a5b3a26
Karibash <y.karibayashi@gmail.com>
2023-03-19 16:21:25 +0900

    Add a frontend server

---
+++ apps/web/src/pages/_document.tsx
@@ -0,0 +1,17 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+import type { FC } from 'react';
+
+const Document: FC = () => {
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+};
+
+export default Document;
